adea4885f0b0572e103790aeeb85a5260cef1e7e
atiehmerikh <at.merikh@gmail.com>
2021-03-29 18:08:31 +0200

    body part numbering file added

---
+++ body_part_reba_calculator/body_part_numbering.py
@@ -0,0 +1,30 @@
+class body_part_number:
+    def neck(self):
+        return [2, 17]
+
+    def trunk_upper_body(self):
+        return [0, 7, 17, 3]
+
+    def right_arm(self):
+        return [7, 8, 9, 10]
+
+    def left_arm(self):
+        return [3, 4, 5, 6]
+
+    # def trunk_lower_body(self):
+    #     return [0, 14, 19, 11]
+
+    def trunk_whole_body(self):
+        return [14, 11, 17, 0]
+
+    def right_leg(self):
+        return [14, 15, 16, 20]
+
+    def left_leg(self):
+        return [11, 12, 13, 19]
+
+    def right_shoulder(self):
+        return [17,7]
+
+    def left_shoulder(self):
+        return [17,3]
